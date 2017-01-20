c1e0bfffe866f42267bf436d33a1fe6e91766533
Alice <alicepergatta@gmail.com>
2017-01-20 13:06:08 +0200

    Add increment LED CLI command function
    - Added increment function, for incrementing brightness
    - Added calibration sign, for calibration values
    - Fixed few bugs in LED_CLI function
    - Still need to re-check argument parsing sequence

--- stm32/Src/LED.cpp
+++ stm32/Src/LED.cpp
@@ -17,10 +17,15 @@ int LED2_EN = 0;
 int LED3_EN = 0;
 int LED4_EN = 0;
 	//Calibration values as addition to hardware calibration
-int LED1_CAL_VALUE = 10;
-int LED2_CAL_VALUE = 10;
-int LED3_CAL_VALUE = 10;
-int LED4_CAL_VALUE = 10;
+int LED1_CAL_VALUE = 1000;
+int LED2_CAL_VALUE = 1000;
+int LED3_CAL_VALUE = 2200;
+int LED4_CAL_VALUE = 1000;
+	//Calibration signs
+char LED1_CAL_SIGN = '-';
+char LED2_CAL_SIGN = '+';
+char LED3_CAL_SIGN = '-';
+char LED4_CAL_SIGN = '-';
 	//Values for external logic
 int LED1_VALUE = 57020;
 int LED2_VALUE = 57020;
@@ -30,18 +35,23 @@ int LED4_VALUE = 57020;
 
 int i = 0;
 int MAX_PWM = 60400;
-int MIN_PWM = 47000;
+int MIN_PWM = 51000;
 
 void char2int(char* pchar, int* pint);
 void LED_SW(void);
 int LEDnumValue(int);
-int LED_CVAL(int VAL, int CAL); 
+int LED_CVAL(int VAL, int CAL, char sign); 
+int LED_ValuebyNum (int lednum);
+int LED_PWMbyNum(int lednum);
+int LED_StatebyNum(int lednum);
 
-int LEDnumValue(int lednum)
+
+int LED_ValuebyNum (int lednum) //Returns corrected PWM value by LED number
 {
 	int value;
 	
-	if((lednum != 0) && (lednum != NULL)) {
+	if ((lednum != 0) && (lednum != NULL)) 
+	{
 		switch (lednum)
 		{
 		case 1:
@@ -63,95 +73,172 @@ int LEDnumValue(int lednum)
 	return value;
 }
 
+int LED_PWMbyNum(int lednum) //Returns direct PWM value by LED number
+{
+	int value;
+	
+	if ((lednum != 0) && (lednum != NULL)) 
+	{
+		switch (lednum)
+		{
+		case 1:
+			value = LED1_PWM;
+			break;
+		case 2:
+			value = LED2_PWM;
+			break;
+		case 3:
+			value = LED3_PWM;
+			break;
+		case 4:
+			value = LED4_PWM;
+			break;
+		default:
+			break;
+		}
+	}
+	return value;
+}
 
-void LED_CLI(char *led_num, char *led_en, char *led_pwm)
+int LED_StatebyNum(int lednum) //Returns ON\OFF state by LED number
+{
+	int value;
+	
+	if ((lednum != 0) && (lednum != NULL)) 
+	{
+		switch (lednum)
+		{
+		case 1:
+			value = LED1_EN;
+			break;
+		case 2:
+			value = LED2_EN;
+			break;
+		case 3:
+			value = LED3_EN;
+			break;
+		case 4:
+			value = LED4_EN;
+			break;
+		default:
+			break;
+		}
+	}
+	return value;
+}
+
+
+void LED_CLI(char *led_num, char *led_en, char *led_pwm) //Command-line interface functions
 {
 	int led_num_int; //LED number variable
 	int led_en_int; //ON\OFF variable
 	int led_pwm_int; //OWM value variable
-//	char2int(led_num, &led_num_int);
-//	char2int(led_en, &led_en_int);
-//	char2int(led_pwm, &led_pwm_int);
+
 	short error = 0; //Error flag
 	
-	short no_en_value = 0; //no convert char to int
-	short no_lednum_value = 0; //no convert char to int
-	short no_pwm_value = 0; //no convert char to int
+	short no_en_value = 0; //no convert char to int flag
+	short no_lednum_value = 0; //no convert char to int flag
+	short no_pwm_value = 0; //no convert char to int flag
 	
-	if (strncmp(led_num, "ALL", 3) == 0 ) { //Set all LED's ON
-		//led_en_int = 1;
+	if (strncmp(led_num, "ALL", 3) == 0 )  //Set all LED's ON
+	{	
 		led_num_int = 228;
 		no_lednum_value = 1;
 		}
-	if (strncmp(led_en, "ON", 2) == 0 ) { //Set LED ON
+	if (strncmp(led_en, "ON", 2) == 0 )  //Set LED ON
+	{
 		led_en_int = 1;
 		no_en_value = 1;
 		}
-	if (strncmp(led_en, "OFF", 3) == 0 ) { //Set LED OFF
+	if (strncmp(led_en, "OFF", 3) == 0 )  //Set LED OFF
+	{
 		led_en_int = 0;
 		no_en_value = 1;
 		}
-	if ((strncmp(led_pwm, "MAX", 3) == 0) && (strncmp(led_num, "ALL", 3) == 0 )) { //If brightness arg is MAX and lednum arg is ALL
+	if ((strncmp(led_pwm, "MAX", 3) == 0) && (strncmp(led_num, "ALL", 3) == 0 ))  //If brightness arg is MAX and lednum arg is ALL
+	{
 		LED1_VALUE = LED2_VALUE = LED3_VALUE = LED4_VALUE = MAX_PWM; //Set all LED's brightness to max
 		led_pwm_int = MAX_PWM;
 		no_pwm_value = 1;
 		}
-	if (strncmp(led_pwm, "MAX", 3) == 0) { //Set LED brightness to max
+	if (strncmp(led_pwm, "MAX", 3) == 0)  //Set LED brightness to max
+	{
 		led_pwm_int = MAX_PWM;
 		no_pwm_value = 1;
 		}
-	if ((strncmp(led_pwm, "MIN", 3) == 0) && (strncmp(led_num, "ALL", 3) == 0 )) { //If brightness arg is MIN and lednum arg is ALL
+	if ((strncmp(led_pwm, "MIN", 3) == 0) && (strncmp(led_num, "ALL", 3) == 0 ))  //If brightness arg is MIN and lednum arg is ALL
+	{
 		LED1_VALUE = LED2_VALUE = LED3_VALUE = LED4_VALUE = MIN_PWM; //Set all LED's brightness to min
 		led_pwm_int = MIN_PWM;
 		no_pwm_value = 1;
 		}
-	if (strncmp(led_pwm, "MIN", 3) == 0) { //Set LED brightness to min
+	if (strncmp(led_pwm, "MIN", 3) == 0)  //Set LED brightness to min
+	{
 		led_pwm_int = MIN_PWM;
 		no_pwm_value = 1;
 		}
-//	if (strncmp(led_pwm, "+", 1) == 0) { //Set LED brightness to min
-//		led_pwm_int = LEDnumValue(led_num) + 10;
-//		no_pwm_value = 1;
-//		}
-//	if (strncmp(led_pwm, "++", 2) == 0) { //Set LED brightness to min
-//		led_pwm_int = LEDnumValue(led_num) + 100;
-//		no_pwm_value = 1;
-//		}
-//	if (strncmp(led_pwm, "+++", 3) == 0) { //Set LED brightness to min
-//		led_pwm_int = LEDnumValue(led_num) + 1000;
-//		no_pwm_value = 1;
-//		}
+	if ((strncmp(led_en, "ON", 2) == 0) && (strncmp(led_num, "ALL", 3) == 0 ))  //If brightness arg is MAX and lednum arg is ALL
+	{
+		LED1_EN = LED2_EN = LED3_EN = LED4_EN = 1; //Set all LED's ON
+		no_en_value = 1;
+		}
+	if ((strncmp(led_en, "OFF", 3) == 0) && (strncmp(led_num, "ALL", 3) == 0 ))  //If brightness arg is MAX and lednum arg is ALL
+	{
+		LED1_EN = LED2_EN = LED3_EN = LED4_EN = 0; //Set all LED's OFF
+		no_en_value = 1;
+		}
+		
+	switch(no_lednum_value) 
+		{
+		case 0:
+		char2int(led_num, &led_num_int);
+		break;
+		case 1:
+		break;
+			default:
+		break;
+		}		
 
-switch(no_pwm_value) 
+	switch(no_en_value) 
+		{
+		case 0:
+		char2int(led_en, &led_en_int);
+		break;
+		case 1:
+		break;
+			default:
+		break;
+		}
+
+	switch(no_pwm_value) 
+		{
+		case 0:
+		char2int(led_pwm, &led_pwm_int);
+		break;
+		case 1:
+		break;
+			default:
+		break;
+		}
+		
+	if (strncmp(led_pwm, "+", 1) == 0)  //
 	{
-	case 0:
-	char2int(led_pwm, &led_pwm_int);
-	break;
-	case 1:
-	break;
-		default:
-	break;
-	}
-switch(no_en_value) 
+		led_pwm_int = LED_ValuebyNum(led_num_int) + 10;
+		no_pwm_value = 0;
+		printf("got + \n\r"); //FIX
+		}
+	if (strncmp(led_pwm, "++", 2) == 0)  //
+	{		
+		led_pwm_int = LED_ValuebyNum(led_num_int) + 100;
+		no_pwm_value = 0;
+		printf("got ++ \n\r"); //FIX
+		}
+	if (strncmp(led_pwm, "+++", 3) == 0)  //
 	{
-	case 0:
-	char2int(led_en, &led_en_int);
-	break;
-	case 1:
-	break;
-		default:
-	break;
-	}
-switch(no_lednum_value) 
-	{
-	case 0:
-	char2int(led_num, &led_num_int);
-	break;
-	case 1:
-	break;
-		default:
-	break;
-	}
+		led_pwm_int = LED_ValuebyNum(led_num_int) + 1000;
+		no_pwm_value = 0;
+		printf("got +++ \n\r"); //FIX
+		}
 
 	switch (led_num_int) //Process argument values
 	{
@@ -188,14 +275,14 @@ switch(no_lednum_value)
 			}
 		break;
 		case 228: //set same parameters for ALL LED's
-			LED1_VALUE = led_pwm_int;
-			LED1_EN = led_en_int;
-			LED2_VALUE = led_pwm_int;
-			LED2_EN = led_en_int;
-			LED3_VALUE = led_pwm_int;
-			LED3_EN = led_en_int;
-			LED4_VALUE = led_pwm_int;
-			LED4_EN = led_en_int;
+//			LED1_VALUE = led_pwm_int;
+//			LED1_EN = led_en_int;
+//			LED2_VALUE = led_pwm_int;
+//			LED2_EN = led_en_int;
+//			LED3_VALUE = led_pwm_int;
+//			LED3_EN = led_en_int;
+//			LED4_VALUE = led_pwm_int;
+//			LED4_EN = led_en_int;
 		break;
 		default:
 			printf("Syntax error near LED \n\r");
@@ -211,20 +298,26 @@ break;
 
 
  
-int LED_CVAL(int VAL, int CAL)  //Function for setting corrected PWM values
+int LED_CVAL(int VAL, int CAL, char sign)  //Function for setting corrected PWM values
 {	
-	int PWM = VAL - CAL;
+	int PWM;
+		if (sign == '-') {
+	PWM = VAL - CAL;
+		}
+		if (sign == '+') {
+	PWM = VAL + CAL;
+		}
   return PWM;
 	}	
 
-void LED_SW() //Set LED's(PM's) on or off by variable values
+void LED_SW() //Set LED's(PM's) parameters
 {
 	
 		//Count PWM values
-LED1_PWM = LED_CVAL(LED1_VALUE, LED1_CAL_VALUE);
-LED2_PWM = LED_CVAL(LED2_VALUE, LED2_CAL_VALUE);
-LED3_PWM = LED_CVAL(LED3_VALUE, LED3_CAL_VALUE);
-LED4_PWM = LED_CVAL(LED4_VALUE, LED4_CAL_VALUE);
+LED1_PWM = LED_CVAL(LED1_VALUE, LED1_CAL_VALUE, LED1_CAL_SIGN);
+LED2_PWM = LED_CVAL(LED2_VALUE, LED2_CAL_VALUE, LED2_CAL_SIGN);
+LED3_PWM = LED_CVAL(LED3_VALUE, LED3_CAL_VALUE, LED3_CAL_SIGN);
+LED4_PWM = LED_CVAL(LED4_VALUE, LED4_CAL_VALUE, LED4_CAL_SIGN);
 	
 	  //Set PWM values into TIM4 registers
 TIM4->CCR1 = LED1_PWM;
